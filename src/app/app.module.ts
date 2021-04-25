977826ef65630ca76e673b59898ea6d38b6d9924
Sarath PS <frndsarath@gmail.com>
2021-04-25 16:49:56 +0530

    new style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { ResumeHomePageComponent } from './resume-home-page/resume-home-page.component';
 
